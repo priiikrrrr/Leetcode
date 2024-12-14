a2493bf8509a8d89364d01c59b82dbc06c00906f
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2024-12-14 16:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (16.76%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int num0 = 0;
-        int num1 = 0;
-        int num2 = 0;
-
-        for(int i = 0; i < nums.size() ; i++){
-            if(nums.at(i)== 0) num0++;
-            else if(nums.at(i)== 1) num1++;
-            else num2++;
+        // one pass -> dutch algo 
+        int low = 0; 
+        int mid = 0;
+        int high = nums.size()-1;
+        //hamesha mid k baare me socho 
+        //0 to low - 1 -> 0th element 
+        // high+ 1 to end -> 2nd element 
+        //low to mid -1 -> 1st element 
+        while(mid <= high){
+            if(nums.at(mid) == 2){
+                int temp = nums.at(mid);
+                nums.at(mid) = nums.at(high);
+                nums.at(high) = temp;
+                high--; 
+            }
+            else if(nums.at(mid) == 0){
+                int temp = nums.at(mid);
+                nums.at(mid) = nums.at(low);
+                nums.at(low) = temp;
+                low++;
+                mid++; 
+            }
+            else //(nums.at(mid) == 1){
+                mid++;
         }
-        for(int i = 0 ; i < nums.size() ; i++){
-            if( i < num0) nums.at(i) = 0 ;
-            else if(i < (num0+num1)) nums.at(i) = 1;
-            else nums.at(i) = 2;
-        }
-        return;
     }
 };
