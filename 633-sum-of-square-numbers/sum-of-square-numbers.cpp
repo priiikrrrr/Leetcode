b023d3a8a9010040849f6ad827ef187b4478115a
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-01-02 13:34:24 +0530

    Time: 6 ms (21.51%) | Memory: 7.9 MB (13.54%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -32,16 +32,17 @@ public:
             return false;
     }
     bool judgeSquareSum(long long c) {
-        // brute force -> O(c)
+        //number of op -> O(rootC)
+        // optimized -> O(rootcC)
         long long x = 0;
         long long y = c;
 
         while (x <= y) {
             if (isperfectSq(x) && isperfectSq(y)) {
                 return true;
-            } else if (!isperfectSq(y)) { //y is greater num ; therefore dec
+            } else if (!isperfectSq(y)) { //y is greater num ; therefore will dec
                 y = (int)sqrt(y) * (int)sqrt(y); // agar y perfect sq nhi hai toh banao usko
-                    x = c - y;
+                    x = c - y; //running root c times max
             } else { // x is not perfect sq // x is smaller -> make it larger --> make it perfect sq
                 x = (int)(sqrt(x) + 1) * (int)(sqrt(x) + 1); // to avoid infinte loop and get an element
                                    // just greater than the number we are at and
@@ -52,3 +53,9 @@ public:
         return false;
     }
 };
+
+/*
+eg c = 102 ..        operations& Tc   ~ O(root C)
+x = 0, 2, 4, 21, 25, 38, 49, 53...
+y = 102, 100, 98, 81, 77, 64, 53, 49...further in rev order 
+*/
