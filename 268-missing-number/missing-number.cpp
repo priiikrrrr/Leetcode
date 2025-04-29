ce134e905c1d68a97743cb4cdc4a41679a16aef1
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-04-29 09:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (14.26%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+    //cyclic sort like approach    
         int n = nums.size();
         int i = 0;
         while(i<n){
@@ -14,3 +15,7 @@ public:
         return n;
     }
 };
+
+//TC O(n)
+//more optimal way?  =>  do sum of 0 to n ; do sum of given array's ele 
+//Then subtract both of em - you'll find the missing ele 
