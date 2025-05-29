712b9248cbe5f71f768c4f5d4a988cc054243fd4
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-05-30 00:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.18%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int gd(int n){
+        for(int i = n/2 ; i >= 1 ; i--){
+            if(n % i == 0)return i;
+        }
+        return 1;
+    }
+
+    bool isPrime(int n){
+        if(n==1)return false;
+        for(int i = 2; i <= sqrt(n) ; i++){//O(sqrt(n))
+            if(n%i==0) return false;
+        }
+        return true;
+}
+    int minSteps(int n) {
+        if(n == 1) return 0;
+        if(isPrime(n)) return n;
+        int count = 0;
+        while(n > 1){
+            int highFactor = gd(n);
+            count += (n/highFactor);
+            n = highFactor;
+        }
+        return count;
+    }
+};
+//hint - 
+//1) think in reverse 
+//2) find current num's highest factor except num 
+/*
+for instance if n = 20 
+1) think in reverse if i have 20 A's how to reach till 1 in optimized way
+2) highest factor of 20 , except 20 is 10 
+--> thought process can be : 
+if i had 10 A's then i couldve doubled it with a copy n paste(20/10) i.e 2 OP 
+then again from 
+1) 10 to 1 optimized way
+2) highest factor 10 , except 10 is 5 
+--> though process can be : 
+if i had 5 A's then i could double it w a copy n paste (10/5) i.e 2 OP 
+then 5 itself prime number 
+therefore number of moves req for a prime num is prime num itself   
+INITIATE A COUNTER = 2 + 2 + 5 = 9 OP (CPPPP,CP,CP)
+*/
