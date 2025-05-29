20a308c4b08c3c8326df752da1afea49b92db547
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-05-30 00:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (56.82%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -7,25 +7,50 @@ public:
         return 1;
     }
 
+    int optimizedGd(int n){
+        for(int i = 2; i <= sqrt(n) ; i++){//this will give us smallest factor 
+        // n/i of smallest factor will give us the largest factor 
+            if(n % i == 0)return n/i;
+        }
+        return 1;
+    }
+
     bool isPrime(int n){
         if(n==1)return false;
         for(int i = 2; i <= sqrt(n) ; i++){//O(sqrt(n))
             if(n%i==0) return false;
         }
         return true;
-}
+    }
+
     int minSteps(int n) {
         if(n == 1) return 0;
-        if(isPrime(n)) return n;
+
+       /* if(isPrime(n)) return n;
         int count = 0;
-        while(n > 1){
-            int highFactor = gd(n);
+        while(n > 1){ //O(logN)
+            int highFactor = optimizedGd(n);
+            count += (n/highFactor);
+            n = highFactor;
+        }
+        */
+        int count = 0;
+        while(n > 1){//O(sqrt(n))
+            if(isPrime(n)){
+                count += n;//O(sqrt(n))
+                break;
+            }
+            int highFactor = optimizedGd(n); //O(sqrt(n))
             count += (n/highFactor);
             n = highFactor;
         }
         return count;
     }
+//hence , this code will have a tc of O((n)) since only one of inner loop will work 
 };
+
+
+
 //hint - 
 //1) think in reverse 
 //2) find current num's highest factor except num 
