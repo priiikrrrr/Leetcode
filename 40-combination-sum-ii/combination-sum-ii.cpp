8b2cd2aa9d39d76d387f82989ad3797903c9d8ae
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-06-03 19:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (31.60%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+    void f(vector<int>& cand, int target,int index, vector<int>&subset){
+        // int orgTarget = target;
+        if(target == 0){
+            result.push_back(subset);
+            return;
+        }
+        if(index == cand.size()) return;
+        //pick 
+        if(cand[index] <= target){
+            subset.push_back(cand[index]);
+            f(cand, target - cand[index], index+1, subset);
+            subset.pop_back();
+            // target = orgTarget;
+        }
+        //not pick
+        int j = index + 1; //to avoid repeated sets, if the ele is same, we will skip making subset/combination from that ele
+        while(j < cand.size() and cand[j] == cand[j-1]) j++;
+        f(cand,target,j,subset);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int>subset;
+        result.clear();
+        f(candidates,target,0, subset);
+        return result;
+    }
+};
