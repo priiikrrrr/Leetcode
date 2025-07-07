f9d1c10417b3dd39581ffdc0b4528e2f85953ea5
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-07-07 10:50:42 +0530

    Time: 19 ms (22.78%) | Memory: 18.5 MB (66.38%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,37 +9,41 @@
  * };
  */
 
- //REFER LEETCODE 21 -> pick up two elements from the last of the array 
- //merge it using that concept , return it and push_back again in the existing array
+// REFER LEETCODE 21 -> pick up two elements from the last of the array
+// merge it using that concept , return it and push_back again in the existing
+// array
 class Solution {
 public:
-    ListNode* merge(ListNode* ta, ListNode* tb){
+    ListNode* merge(ListNode* ta, ListNode* tb) {
         ListNode* dummy = new ListNode(100);
         ListNode* tc = dummy;
 
-        while(ta != NULL && tb != NULL){
-            if(ta->val <=  tb->val){
+        while (ta != NULL && tb != NULL) {
+            if (ta->val <= tb->val) {
                 tc->next = ta;
                 ta = ta->next;
                 tc = tc->next;
-            }else{
+            } else {
                 tc->next = tb;
                 tb = tb->next;
                 tc = tc->next;
             }
         }
-        if(ta == NULL)tc->next = tb;
-        else tc->next = ta;
+        if (ta == NULL)
+            tc->next = tb;
+        else
+            tc->next = ta;
         return dummy->next;
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size() == 0)return NULL;
-        while(lists.size() > 1){
-            ListNode* ta = lists[lists.size() -1];
-            lists.pop_back();
-            ListNode* tb = lists[lists.size() -1];
-            lists.pop_back();
-            ListNode* tc = merge(ta,tb);
+        if (lists.size() == 0)
+            return NULL;
+        while (lists.size() > 1) {
+            ListNode* ta = lists[0];
+            lists.erase(lists.begin());//pop- front
+            ListNode* tb = lists[0];
+            lists.erase(lists.begin());
+            ListNode* tc = merge(ta, tb);
             lists.push_back(tc);
         }
         return lists[0];
