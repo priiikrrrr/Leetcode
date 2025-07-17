85cc43857c7e70ee8848d7f4b0acde6f0f4f651b
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-07-17 18:52:53 +0530

    Time: 9 ms (8.92%) | Memory: 23.6 MB (22.94%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,10 +1,51 @@
 class MinStack {
 public:
 /*
+NO ex ds is used - just dummy variables 
+SC O(1), TC O(1)
+*/  
+    stack<long long>st;
+    long long min ;
+    MinStack() {//constructor   
+        min = LLONG_MAX;
+    }
+    void push(int val) {//O(1)
+    long long x = (long long)val;
+        if(st.size() == 0){
+            st.push(x);
+            min = x;
+        }
+        else if(x >= min)st.push(x);
+        else{//val < min
+            st.push(2*x - min);//checkmark min
+            min = x;
+
+        }
+    }
+    
+    void pop() { //O(1)
+        if(st.top() >= min)st.pop();
+        else{
+            //st.top() < min -> checkmark value is here, i.e before popping the top ele , we have to retrive the prev min  & update the curr to prev min
+            long long oldMin = 2*min - st.top();
+            min = oldMin;
+            st.pop();
+        }
+    }
+    
+    int top() {//O(1)
+        if(st.top() < min)return (int)min;
+        else return (int)(st.top());
+    }
+    
+    int getMin() {//O(1)
+        return (int)min;
+    }
+};
+/*
 vector ko as stack implement karo & each time you call get min -> O(n)
 you have to traverse the entire vector
 SC O(1), TC O(n)
-*/
     vector<int>v;
     MinStack() {//constructor   
     }
@@ -28,6 +69,7 @@ SC O(1), TC O(n)
         return mn;
     }
 };
+*/
 
 
 
