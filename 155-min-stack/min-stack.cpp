2e1ca668d29c6167f440645ca0bea7fb2c6142aa
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-07-17 18:32:25 +0530

    Time: 48 ms (5.03%) | Memory: 23 MB (99.93%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,5 +1,41 @@
 class MinStack {
 public:
+/*
+vector ko as stack implement karo & each time you call get min -> O(n)
+you have to traverse the entire vector
+SC O(1), TC O(n)
+*/
+    vector<int>v;
+    MinStack() {//constructor   
+    }
+    void push(int val) {//O(1)
+        v.push_back(val);
+    }
+    
+    void pop() { //O(1)
+        v.pop_back();
+    }
+    
+    int top() {//O(1)
+        return v[v.size() - 1];
+    }
+    
+    int getMin() {//O(1)
+        int mn = v[0];
+        for(int i = 1 ; i < v.size() ; i++){
+            mn = min(mn, v[i]);
+        }
+        return mn;
+    }
+};
+
+
+
+/*
+METHOD 2 - using extra stack that will always have the same no. of ele as the "st"
+TC O(1) SC O(n)
+class MinStack {
+public:
     stack<int>st;
     stack<int>helper;
     MinStack() {//constructor   
@@ -26,8 +62,11 @@ public:
         return helper.top();
     }
 };
+*/
 
-/**
+
+
+/*
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
  * obj->push(val);
@@ -37,7 +76,19 @@ public:
  */
 
  /*
-getMin()-> brute force approach Using extra stack that will always have  the same   number of ele as your given st 
-
- 
+getMin()-> brute force approach Using extra stack :TLE
+    getMin(){
+        stack<int>helper;
+        int mn = INT_MAX;
+        while(st.size() > 0){
+            mn = min(mn, st.top());
+            helper.push(st.top());
+            st.pop();
+        }
+        while(helper.size() > 0){
+            st.push(helper.top());
+            helper.pop();
+        }
+        return mn;
+    }
  */
