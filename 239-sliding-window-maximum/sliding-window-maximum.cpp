126dea369a55492656ff9535ec3e20accf72172a
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-07-20 16:34:55 +0530

    Time: 18 ms (79.65%) | Memory: 139.2 MB (60.32%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,53 +1,17 @@
 class Solution {
-    //!! redo
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        //num of windows = n - k + 1
-        if(k == 1) return arr;
-        int n = arr.size();
-        int ngi[n];
-        stack<int>st;
-        ngi[n-1] = n;
-        st.push(n-1);
-
-    for(int i  = n-2 ; i >= 0 ; i--){
-        while(st.size()>0 && arr[st.top()] <= arr[i]) st.pop();
-        if(st.size() == 0)ngi[i] = n;
-        else ngi[i] = st.top();
-        st.push(i);
-    }
-        vector<int>ans;
-        int j = 0;
-        for(int i = 0 ; i < (n - k + 1) ; i++){
-            if(j < i) j = i;
-            int mx = arr[j];
-            while(j < (i+k)){
-                mx = arr[j]; //mark ans
-                if(ngi[j] >= i+k)break;
-                j  = ngi[j]; //go to next
-            }
-            ans.push_back(mx);
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(k == 1) return nums;
+        int n = nums.size();
+        deque<int>dq;//dec order of ele. (we are pushing idex)
+        vector<int>v;
+        for(int i = 0 ; i < n ; i++){
+            while(dq.size() > 0 && nums[i] > nums[dq.back()])dq.pop_back();
+            dq.push_back(i);
+            int j = i - k + 1;//start of the window youre at; //imp
+            while(dq.front() < j)dq.pop_front();//imp
+            if(i >= (k - 1))v.push_back(nums[dq.front()]);
         }
-        return ans;
+        return v;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-        //BF
-        // for(int i = 0 ; i < (n-k+1) ; i++){
-        //     int mx = INT_MIN;
-        //     for(int j = i ; j < (i+k) ; j++){
-        //         mx = max(mx, arr[j]);
-        //     }
-        //     nge.push_back(mx);
-        // }
-        // return nge;
