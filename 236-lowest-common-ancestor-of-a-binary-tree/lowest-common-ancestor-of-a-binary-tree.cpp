29f1762e966566a72d7db48833c834976a972483
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-07-21 20:24:48 +0530

    Time: 572 ms (5.12%) | Memory: 17.1 MB (97.70%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -6,10 +6,11 @@ public:
         return find(root->left, value)|| find(root->right, value);
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == p || root == q) return root;
-        else if(find(root->left, p) && find(root->right, q))return root;
-        else if(!find(root->left, p) && !find(root->right, q))return root;
-        else if(find(root->left, p) && !find(root->right,q))return lowestCommonAncestor(root->left, p , q);
-        else return lowestCommonAncestor(root->right, p , q);
+        // if(root == p || root == q) return root;
+        // if(find(root->left, p) && find(root->right, q))return root;
+        // else if(find(root->right, p) && find(root->left, q))return root;
+        if(find(root->left, p) && find(root->left,q))return lowestCommonAncestor(root->left, p , q);
+        else if(find(root->right, p) && find(root->right,q))return lowestCommonAncestor(root->right, p , q);
+        return root;
     }
 }; 
