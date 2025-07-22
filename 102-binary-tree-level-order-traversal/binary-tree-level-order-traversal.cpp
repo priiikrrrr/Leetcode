1267c7c4dd168cd14deb896850169f9e16660583
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-07-22 15:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (97.53%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
+//optimized
     int levels(TreeNode* root){
         if(root == NULL)return 0;
         return 1+ max(levels(root->left), levels(root->right));
     }
-    void PrintTraversal(TreeNode* root , int currlevel , int reqLevel, vector<int>& v){
+    void order(TreeNode* root, vector<vector<int>>& final, int l){
         if(root == NULL)return;
-        if(currlevel == reqLevel){
-            v.push_back(root->val);
-            return;
-        }
-        PrintTraversal(root->left, currlevel+1 , reqLevel , v);
-        PrintTraversal(root->right, currlevel+1 , reqLevel , v);
-    }
+        final[l].push_back(root->val);
 
-    void order(TreeNode* root, vector<vector<int>>& final){
-        int n = levels(root);
-        for(int i = 1 ; i <= n ; i++){
-            vector<int>v;
-            PrintTraversal(root,1,i,v);
-            final.push_back(v);
-            cout << endl;
-        }
+        order(root->left, final, l+1);
+        order(root->right, final, l+1);
     }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>final;
-        order(root, final);
+        int n = levels(root);
+        vector<vector<int>> final;
+        for(int i = 1 ; i <= n ; i++){
+            vector<int> v;
+            final.push_back(v);
+        }
+        order(root, final, 0);
         return final;
     }
 };
