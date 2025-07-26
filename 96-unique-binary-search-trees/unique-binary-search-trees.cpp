e46eecf16e9f3453b9f7bad63bec81b536a83ea4
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-07-26 22:34:39 +0530

    Time: 2033 ms (9.15%) | Memory: 7.7 MB (98.39%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if(n <= 1)return 1;
+        if(n == 1 || n == 0)return 1;
         int res = 0; 
         for(int i = 1 ; i <= n ; i++){
             res += numTrees(i - 1) * numTrees(n-i);
