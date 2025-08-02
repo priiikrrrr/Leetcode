3233fa0730381f6391a132519c7882854bdcfb0a
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-02 11:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (45.90%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    long long maxTree(TreeNode* root){
+    long long findMax(TreeNode* root){
         if(root == NULL)return LLONG_MIN;
-        return max((long long)(root->val) , max(maxTree(root->left), maxTree(root->right)));
+        return max((long long)(root->val), max(findMax(root->left), findMax(root->right)));
     }
-    long long  minTree(TreeNode* root){
+    long long findMin(TreeNode* root){
         if(root == NULL)return LLONG_MAX;
-        return min((long long)(root->val), min(minTree(root->left), minTree(root->right)));
-    }
-    bool isValidBst(TreeNode* root){
-        if(root == NULL) return true;
-        else if((long long)(root->val) <= maxTree(root->left))return false;
-        else if((long long)(root->val) >= minTree(root->left))return false;
-        return isValidBst(root->left) && isValidBst(root->right);
-    
-    }
-    void inorder(TreeNode* root, vector<int>&ans){
-        if(root == NULL) return; //since bst is always in sorted order (inorder traversal)
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
+        return min((long long)(root->val), min(findMin(root->left), findMin(root->right)));
     }
     bool isValidBST(TreeNode* root) {
-        vector<int>ans; 
-        inorder(root, ans);
-        for(int i = 1 ; i < ans.size() ; i++){
-            if(ans[i] <= ans[i-1]) return false;
-        }
-        return true;
+        if(root == NULL)return true;
+        else if((long long)(root ->val) <= findMax(root->left))return false;
+        else if((long long)(root ->val) >= findMin(root->right))return false;
+        return isValidBST(root->left) && isValidBST(root->right);
+        
     }
 };
