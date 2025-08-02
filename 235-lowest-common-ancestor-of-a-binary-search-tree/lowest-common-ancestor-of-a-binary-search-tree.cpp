728445d06a775e9098d34ec596f6a57473952f77
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-02 10:26:45 +0530

    Time: 23 ms (53.91%) | Memory: 23.5 MB (64.34%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,32 +1,11 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p, q);
-        else if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left, p, q);
-        else return root;
-    }
-}; 
-
- /*
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root->val == p->val || root->val == q->val)return root;
-        else if(root->val > p->val && root->val < q->val)return root;
-        else if(root->val < p->val && root->val > q->val)return root;
-        else if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p, q);
-        else return lowestCommonAncestor(root->left, p, q);
+        else if(root ->val > p->val && root->val < q->val)return root; 
+        else if(root ->val < p->val && root->val > q->val)return root;
+        else if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p , q);
+        //root->val > p->val && root->val > q->val
+        else return lowestCommonAncestor(root->left, p , q);
     }
- */   
-/*
-    bool find(TreeNode* root , TreeNode* value){
-        if(root == NULL)return false;
-        if(root == value) return true;
-        return find(root->left, value)|| find(root->right, value);
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-
-        if(find(root->left, p) && find(root->left,q))return lowestCommonAncestor(root->left, p , q);
-        else if(find(root->right, p) && find(root->right,q))return lowestCommonAncestor(root->right, p , q);
-        return root;
-    }
-*/
-
+};
