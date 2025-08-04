427e1425cf15c56323f2b86a5381c0aba72c1f5e
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-04 18:23:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -12,10 +12,14 @@ public:
         for(int i = 0 ; i < t.length() ; i++){
             m2[t[i]]++;
         }
+        //map1  k har ele p gye
         for(auto x : m1){
+            //key aur value store ki
             char ch1 = x.first;
             int frq1 = x.second;
+            //map2 main respective key dhundhi
             if(m1.find(ch1) != m2.end()){
+                //& then compare ki 
                 int frq2 = m2[ch1];//gives the value of key, i.e inc of frq
                 if(frq1 != frq2)return false;
             }
