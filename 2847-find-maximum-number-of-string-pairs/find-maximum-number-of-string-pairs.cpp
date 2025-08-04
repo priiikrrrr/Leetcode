66702c9cc921e8eb274dda5357047ba46a95d6e7
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-04 16:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (33.33%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
@@ -5,18 +5,10 @@ public:
         int count = 0;
         unordered_set<string>s;
         for(int i = 0 ; i < n ; i++){ //O(n)
-        //Set mein ele dalo
-            s.insert(arr[i]);
-        //array ke ele ko reverse karo ; taaki jo ele same hain , wo same dikh jaye on 
-        //traversal hence helping with comparision in set for unique ele
             string rev = arr[i];
             reverse(rev.begin(), rev.end());
-        //after reversal comparision
-            if(arr[i] == rev)continue;
-            if(s.find(rev) != s.end()){ //O(1)
-                count++;
-                s.erase(arr[i]);
-            }
+            if(s.find(rev) != s.end())count++;
+            else s.insert(arr[i]);
         }
         return count;
     }
