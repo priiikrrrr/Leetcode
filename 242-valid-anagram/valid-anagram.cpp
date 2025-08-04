c842d827ea11ca3af7f470c643d3101ec1ebd539
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-04 18:16:59 +0530

    Time: 2 ms (56.66%) | Memory: 9.8 MB (26.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s == t)return true;
-        else return false;
+        //int -> frequency 
+        //char is string's char
+        if(s.length() != t.length())return false;
+        unordered_map<char,int>m1; 
+        unordered_map<char,int>m2;
+        for(int i = 0 ; i < s.length() ; i++){
+            m1[s[i]]++;
+        } 
+        for(int i = 0 ; i < t.length() ; i++){
+            m2[t[i]]++;
+        }
+        for(auto x : m1){
+            char ch1 = x.first;
+            int frq1 = x.second;
+            if(m1.find(ch1) != m2.end()){
+                int frq2 = m2[ch1];//gives the value of key, i.e inc of frq
+                if(frq1 != frq2)return false;
+            }
+            else return false;
+        }
+        return true;
     }
 };
