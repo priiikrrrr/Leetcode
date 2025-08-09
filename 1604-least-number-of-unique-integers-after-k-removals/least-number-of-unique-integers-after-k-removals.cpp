5b45d447951172432d7c18fc05d7f5b240f65742
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-09 14:32:52 +0530

    Time: 99 ms (91.49%) | Memory: 66.5 MB (73.10%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -3,9 +3,12 @@ public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         priority_queue<int, vector<int>, greater<int>>q;//min heap
         unordered_map<int,int>mp;
+        //storing the freq of the numbers given in the array (hint use)
         for (int num : arr) mp[num]++;
+        //storing the freq of the numbers itself in the minheap so we dont have to go thru each ele in the heap, 
+        //simply looking at the freq will give us the indication of how many uniq ele there might be present 
         for(auto &i : mp)q.push(i.second);
-        //storing the ele and their frequencies in map 
+        //while heap aint empty && the number of ele to be removed(k) >= the freq of the ele at the top of heap we keep removing ele(hint use)
         while(!q.empty() && k >= q.top()){
             k = k - q.top();//since q stores occurences not, numbers
             q.pop();
