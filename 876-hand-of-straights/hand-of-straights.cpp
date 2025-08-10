bc981e3f5a4159be27b216aa8c2abe09a06ad26a
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-10 19:18:25 +0530

    Time: 40 ms (48.79%) | Memory: 32.3 MB (38.45%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) {
+        if (nums.size() % k != 0) return false;
+        map<int, int> mp; // ordered map for smallest->largest access
+        
+        for (int i : nums)mp[i]++;
+
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            int i = it->first;  //key : number
+            int j = it->second; //freq of number
+            if (j > 0) {        //when freq > 0
+                for (int t = 0; t < k; t++) {
+                    int key = i + t;
+                    if (mp[key] < j) return false;
+                    mp[key] -= j;
+                }
+            }
+        }
+        return true;
+    }
+};
