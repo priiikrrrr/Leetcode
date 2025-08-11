e7f3823e91cc29adda34edaef322362d74508500
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-11 20:18:48 +0530

    Time: 7 ms (20.11%) | Memory: 16.9 MB (7.99%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,27 +1,17 @@
-struct customcomp{ //overloading max heap for freq, based seggregation
-    bool operator()(pair<char,int> &a, pair<char,int> &b) {
-        return a.second < b.second; 
-    }    
-};
-
-
-// TC : O(NlogN);
-// SC : O(N)
+// TC : O(N);
+// SC : O(N)using bucket sort : assis.
 class Solution {
 public:
     string frequencySort(string s) {
-        priority_queue<pair<char,int>, vector<pair<char,int>>, customcomp> pq;//max heap
         unordered_map<char, int>mps;
         for(char i : s)mps[i]++;
-        for(auto &i : mps)pq.push({i.first, i.second});
+
+        vector<vector<char>>sort(s.size() + 1);
+        for(auto &i : mps)sort[i.second].push_back(i.first);
+
         string temp = "";
-        while(!pq.empty()){
-            // temp.push_back(pq.top().first);
-            // pq.pop();
-            char ch = pq.top().first;
-            int chfreq = pq.top().second;
-            temp.append(chfreq, ch);// jitni freq hai ek char ki utne times , us character ko append krdo
-            pq.pop(); 
+        for(int i = s.size() ; i > 0 ; i--){
+            for(char t : sort[i])temp.append(i , t); //append i times , t 
         }
         return temp;
     }
