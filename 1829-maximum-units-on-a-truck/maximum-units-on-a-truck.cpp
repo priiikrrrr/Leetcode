8f99a5567351a698439943057a114fd3039940d9
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-13 21:01:10 +0530

    Time: 4 ms (56.38%) | Memory: 19.9 MB (47.51%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -0,0 +1,24 @@
+bool customComp(vector<int>&a, vector<int>&b){
+    return a[1]>b[1];//jiska profit zyada hai woh pehle 
+    //i.e if a ka profit zyada hai toh a pehle uthega ...so on
+}
+
+//tc : o(NlogN) sc : O(sort);
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), customComp);
+        int profit = 0;
+        for(int i = 0 ; i < boxTypes.size() ; i++){
+            if(boxTypes[i][0] <= truckSize){
+                profit += boxTypes[i][0] * boxTypes[i][1];
+                truckSize -= boxTypes[i][0];
+            }else{
+                profit += truckSize*boxTypes[i][1];
+                truckSize = 0;
+            }
+            if(truckSize == 0)break;
+        }
+        return profit;
+    }
+};
