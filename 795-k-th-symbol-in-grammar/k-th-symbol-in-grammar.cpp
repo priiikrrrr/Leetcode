adef322367ba81e542b45e1161c8cd00b49be5f4
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-08-23 20:59:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (81.24%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-//TC => O(n)
+    int mypow(int n , int k){
+        if(k == 0)return 1;
+        int half = mypow(n , k/2);
+        if(k % 2 == 0)return half*half;
+        else return half * half * n;
+    }
     int kthGrammar(int n, int k) {
-        if(n == 1)return 0;
-        if((k % 2 == 0)){
-            //flip answer from root at (k/2)
-            int prevAns = kthGrammar(n-1,k/2);
-            if(prevAns == 0) return 1;
-            else return 0;
+        //base
+        if(n <= 1 && k == 1)return 0;
+        //generating mid using expo condition
+        int mid = mypow(2,n-1)/2;
+        if(k <= mid){
+            return kthGrammar(n-1,k);
         }else{
-            // k is odd
-            int prevAns = kthGrammar(n-1,k/2+1);
-            //same ans from root , at (k/2 + 1)
-            return prevAns;
+            return 1 - kthGrammar(n-1, k-mid);
         }
     }
 };
