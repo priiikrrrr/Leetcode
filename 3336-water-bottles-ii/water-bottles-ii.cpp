d8e38401d47834febe3836e731f0928195dd931c
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-10-02 18:29:18 +0530

    Time: 5 ms (21.95%) | Memory: 8.5 MB (77.56%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.cpp
+++ 3336-water-bottles-ii/water-bottles-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxBottlesDrunk(int numBottles, int numExchange) {
-        int db = 0; 
+        int db = 0;
         int eb = 0;
+
         while(eb >= numExchange || numBottles != 0){
             if(eb < numExchange){
-                eb += numBottles;
                 db += numBottles;
+                eb += numBottles;
                 numBottles -= numBottles;
             }else{
-                //eb > numExchange;
                 while(eb >= numExchange){
                     eb = eb - numExchange;
                     numExchange++;
