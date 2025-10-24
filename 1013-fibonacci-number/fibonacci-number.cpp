21c471979fa0ec639ffea7434f0cc01198ca7719
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-10-24 19:51:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.97%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
-public:
+public://tc : o(n) sc : o(n)
+int fibn(int n , vector<int>&t){
+    if(n <= 1)return n;
+    if(t[n] != -1) return t[n];
+    // int ans =  fibn(n-1) + fibn(n-2);
+    // return ans;
+    t[n] = fibn(n-1, t) + fibn(n-2, t);
+    return t[n];
+}
     int fib(int n) {
-        if(n <= 1) return n;
-        return fib(n-1) + fib(n-2);
+        vector<int>t(n+1, -1);
+        return fibn(n , t);
     }
 };
