8a3ce784a0b904a171ffe9d635bf69af7ead69a1
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-10-24 12:47:07 +0530

    Time: 167 ms (5.19%) | Memory: 9.5 MB (96.11%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-//     bool score(vector<int>& nums, int lastVal){
-//         // int count = 0;
-//         // if(nums.size() == 0 || nums[nums.size() - 1] > lastVal)count += nums[nums.size() -1];
-//         // int count1 = 0;
-//         // int val = [nums.size() - 1];
-//         // nums.pop_back();
-//         // score(nums, lastVal);
-//         // count1 += count1;
-
-//         // if(count > count1)return true;
-//         // else return false;
-//         if(i > j)return 0;
-//         return total - min(winnerScore(nums, total - nums[i], i+1, j), winnerScore())
-//     }
-//     bool predictTheWinner(vector<int>& nums) {
-//         if(nums.size() <= 1)return true;
-//         // int lastVal = nums[nums.size() - 1];
-//         // int total = 0;
-//         // total += lastVal;
-//         // nums.pop_back();
-//         // predictTheWinner(nums);
-//         // score(nums, lastVal, total); 
-//         int total = 0;
-//         for(auto i : nums)total += i;
-//         return score(winnerScore(nums, total, 0 , nums.size() - 1), total);
-// if(i > j)return winner(nums, i , j);
-    bool score(vector<int>& nums, int i , int j , int scoreA, int scoreB, bool Current_turn){
+    bool solve(vector<int>& nums, int scorea, int scoreb , int i, int j ,  bool turn){
         if(i > j){
-            if(scoreA >= scoreB)return true;
-            else return false;
+            return scorea >= scoreb;
         }
-        if(Current_turn)
-            return score(nums, i+1 , j , scoreA + nums[i] , scoreB , false) || score(nums, i , j-1 , scoreA + nums[j] , scoreB , false);
-            return score(nums, i+1 , j , scoreA  , scoreB + nums[i], true) && score(nums, i , j-1 , scoreA , scoreB+ nums[j] , true);
+        // if(n <= 2)return true;
 
+        if(turn){
+            //picking up from the start & end of the list for a
+            bool scoreofA =   solve(nums , scorea +nums[i] , scoreb , i+1 , j, false);
+            bool scoreofA2 =  solve(nums , scorea+ nums[j] , scoreb  , i, j-1 , false);
+            return scoreofA || scoreofA2;
+        }else{
+            //picking up from the start & end of the list for b 
+            bool scoreofB =  solve(nums , scorea  , scoreb + nums[i] , i+1 , j , true);
+            bool scoreofB2 = solve(nums , scorea  , scoreb + nums[j] , i , j-1 , true);
+            return scoreofB && scoreofB2;
+        }
     }
     bool predictTheWinner(vector<int>& nums) {
-        // int scoreA = 0;
-        // int scoreB = 0;
-        return score(nums, 0 , nums.size() - 1, 0 , 0, true);
+        return solve(nums, 0 , 0 , 0 , nums.size() -1 , true);
     }
 };
