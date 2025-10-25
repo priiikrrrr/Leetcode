a2f1afc8bbd3f107cb52784a5e99c05983ec6c95
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-10-25 10:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (96.80%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -3,13 +3,23 @@ public:
 int help(int sr, int sc, int er, int ec ,vector<vector<int>>&dp){
     if(sr == er & sc == ec)return 1;
     if(sr > er || sc > ec)return 0;
-    if(dp[sr][sc] != -1)return dp[sr][sc];
-    int right = help(sr, sc + 1, er , ec,dp);
+    if(dp[sr][sc] != -1)return dp[sr][sc]; //memoize
+    int right = help(sr, sc + 1, er , ec,dp);//recursion
     int down = help(sr + 1, sc, er , ec,dp);
-    return dp[sr][sc]  = right + down;
+    return dp[sr][sc]  = right + down; //rec + memoi
 }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        return help(0 , 0 , m - 1, n -1, dp);
+        // vector<vector<int>>dp(m, vector<int>(n, -1));
+        // return help(0 , 0 , m - 1, n -1, dp);
+
+        //tabulation method
+        int dp[m][n]; //sc = O(mn)
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 || j == 0)dp[i][j] = 1;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
