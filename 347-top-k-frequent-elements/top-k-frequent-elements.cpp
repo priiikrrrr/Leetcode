73e9174573810d0da087e10da8237bcc9ec7ebf6
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-01 20:48:23 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (9.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,20 +1,21 @@
-struct customcomp{
-    bool operator()(pair<int,int> &a, pair<int,int> &b) {
-        return a.second < b.second; 
-    }    
-};
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int>mp;
+        vector<vector<int>>bck(nums.size() + 1);
+        for(auto i : nums)mp[i]++;
+        for(auto i : mp){
+            int num = i.first;
+            int frq = i.second;
+            bck[frq].push_back(num);
+        }
         vector<int>ans;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, customcomp> pq;
-        unordered_map<int,int>mp;
-        for(int i : nums)mp[i]++;
-        for(auto &i : mp)pq.push({i.first, i.second});
-        while(k){
-            ans.push_back(pq.top().first);
-            pq.pop();
-            k--;
+        for(int i = bck.size() - 1; i >= 0 && ans.size() < k ; i--){
+            for(int j : bck[i]){
+                // here j is the number which has appeared bck[i]times. since back[i] is the frq itself
+                ans.push_back(j);
+                if(ans.size() == k)break;
+            }
         }
         return ans;
     }
