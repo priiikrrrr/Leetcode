32f5949469070d4334e5e6134dbcd75071f9d13e
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-03 17:51:57 +0530

    Time: 31 ms (22.61%) | Memory: 34.8 MB (15.05%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -24,3 +24,31 @@ public:
         return {dupe, reqMiss};
     }
 };
+
+/*
+
+SIMULATION CODE : 
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        std::ranges::sort(nums);
+        vector<int> res(2,0);
+        for(int i=0;i<nums.size();i++){
+            if(i==0){
+                if(nums[0]!=1){
+                    res[1] = 1;
+                }
+                continue;
+            }
+            if(nums[i]==nums[i-1]){
+                res[0] = nums[i];
+            }else if(nums[i]!=nums[i-1]+1){
+                res[1] = nums[i-1]+1;
+            }
+        }
+        if(res[1]==0) res[1] = nums.size();
+        return res;
+    }
+}
+
+*/
