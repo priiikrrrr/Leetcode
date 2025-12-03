84d063a218372afc98b9aaa97452b6a73adeb4fc
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-03 18:20:27 +0530

    Time: 12 ms (38.01%) | Memory: 11.8 MB (51.79%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,35 +9,16 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-       /*
-        here we are replacing value of ll which isnot reliable since we are 
-        assigning the bool values to int, and its against the q as well 
-       
-        ListNode*temp = head;
-        // ListNode*end = NULL;
-        // int len = 0;
-        bool flag = false;
-        if(temp == NULL)return false;
-        while(temp!= NULL){
-            if(temp->val == true) return true;
-            else{
-            temp->val = true;
-            temp = temp->next;
-            }
+        if(head == NULL || head->next == NULL)return false;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)return true;
         }
         return false;
-       */
-//better approach is to use floyd tortoise;hare algo // fast slow algo 
-       if(head == NULL || head->next == NULL) return false;
-       ListNode* temp1 = head; 
-       ListNode* temp2 = head;
-        while(temp2 != NULL && temp2->next != NULL){
-            temp1 = temp1->next;
-            temp2 = temp2->next->next;
-
-            if(temp1 == temp2)return true;//cycle detected
-        }
-       return false;
     }
 };
