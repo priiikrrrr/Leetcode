da058623269151d326890788fc8ca136faa38324
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-07 19:26:51 +0530

    Time: 3 ms (33.49%) | Memory: 10 MB (57.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int i = 0 ; 
-        int j = s.size() -1;
+        int j = s.size() - 1; 
         while(i < j){
             if(!isalnum(s[i]))i++;
             else if(!isalnum(s[j]))j--;
