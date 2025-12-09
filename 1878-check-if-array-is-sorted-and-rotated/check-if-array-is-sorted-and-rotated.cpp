3d515631ba472af8082f7a9ed9d1d8d534d11776
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-09 10:59:38 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (95.54%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
+        //sliding window approach
         int n = nums.size();
-        if(n <= 1) return true;
-
-        int invCount = 0;
-        for(int i = 1 ; i < n ; i++){
-            if(nums[i] < nums[i-1]){
-                ++invCount;
-                if(invCount > 1) return false;
-            }
+        if(n <= 1)return true;
+        int c  = 1 ;
+        for(int i = 1 ; i < 2*n ; i++){
+            if(nums[(i - 1) % n] <= nums[i % n])c++;
+            else c = 1;
+            if(c == n)return true;
         }
-        if(nums[0] < nums[n -1])++invCount;
-
-        if(invCount <= 1)return true;
-        else return false;
+        return false;
     }
 };
