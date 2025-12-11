637bc70bb2dcbd43a2d7fba6d9fd3769e08ad345
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-11 16:30:31 +0530

    Time: 13 ms (18.71%) | Memory: 145.1 MB (9.00%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,28 +2,21 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         if(nums.size()== 1 || nums.empty()) return nums;
-        vector<int>ans;
-        vector<int>pos,neg;
-        for(int num: nums){
-            if(num >= 0) pos.push_back(num);
-            else neg.push_back(num);
-            }
-        int i = 0; 
+        vector<int>pos;
+        vector<int>neg;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] >= 0)pos.push_back(nums[i]);
+            else neg.push_back(nums[i]);
+        }
+        vector<int>res;
+        int i = 0 ; 
         int j = 0;
         while(i < pos.size() && j < neg.size()){
-            ans.push_back(pos[i]);
+            res.push_back(pos[i]);
             i++;
-            ans.push_back(neg[j]);
+            res.push_back(neg[j]);
             j++;
         }
-        return ans;
+        return res;
     }
 };
-
-//to think :: 
-/*
-what is needed - separation of arrays 
-what to repeat - alternate num 
-edge cases - empty arr, size 1, odd num (counter to this ques), 2 neg , 1 pos(or vice versa)(counter to the q)
-since the ques said equal num of neg and pos hence last two edge cases are nullified
-*/
