aeaeaeda4e714143938c84d2fabb80b6e09f76d8
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-21 16:31:21 +0530

    Time: 7 ms (16.70%) | Memory: 20 MB (10.47%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int>mp;
-        vector<vector<int>>bck(nums.size() + 1);
-        for(auto i : nums)mp[i]++;
-        for(auto i : mp){
-            int num = i.first;
-            int frq = i.second;
-            bck[frq].push_back(num);
+        //map the frequency
+        unordered_map<int, int> mp;
+        for (const int& i : nums)
+            mp[i]++;
+        //initialize the array to map the number of freq to the index , and the stored value in the vector to be the value of that particular index 
+        vector<vector<int>> result(nums.size() + 1);
+        //in the index of result push the value(index being the frequency of the value to be pushed)
+        for (auto k : mp) {
+            result[k.second].push_back(k.first);
         }
-        vector<int>ans;
-        for(int i = bck.size() - 1; i >= 0 && ans.size() < k ; i--){
-            for(int j : bck[i]){
-                // here j is the number which has appeared bck[i]times. since back[i] is the frq itself
-                ans.push_back(j);
-                if(ans.size() == k)break;
+        vector<int> ans;
+        //initialize another vec , traverse result from right to left, because indices are in sorted order ofc(0,1,2,3...)and here indices are the frequency 
+        for (int j = result.size() - 1; j > 0; j--) {
+            //while each index can have multiple element with the same freq , therefore, 
+            //for each ele go inside that ele as well and push it to the ans. to find the top k 
+            for (int l : result[j]) {
+                ans.push_back(l);
             }
+            if(ans.size() == k)return ans;
         }
         return ans;
     }
