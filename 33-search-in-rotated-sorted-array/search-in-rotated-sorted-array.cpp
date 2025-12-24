364a05851f6e5cf810dac69c75f1c70804b4ec89
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-24 13:40:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (65.32%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,82 +1,43 @@
 class Solution {
 public:
-    int search(vector<int>& v, int target) {
-        int n = v.size();
-        int low = 0;
-        int high = n - 1;
-        //if array is of 2 < size 
-        if(n==2){
-            if(target == v.at(0)) return 0;
-            else if(target == v.at(1)) return 1;
+//observation  : the left half is not sorted & the right half is sorted since rotating array : will move the ele towards the beginning making the left side of the array unsorted yet the non rotated part i.e the right part is still sorted 
+    int search(vector<int>& nums, int target) {
+        if (nums.size() == 2) {
+            if (target == nums[0])
+                return 0;
+            else if (target == nums[1])
+                return 1;
             else return -1;
         }
-        int pivot = -1; // smallest ele
-        // finding pivot
-        while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            //when array size < 2 edge cases : 
-            if(mid == 0 ) low = mid + 1;
-            else if(mid == n-1) high = mid - 1;
-            //to find pivot in general array
-            else if (v.at(mid) < v.at(mid + 1) &&
-                v.at(mid) < v.at(mid - 1)) { // pivot
-                pivot = mid;
-                break;
-            } else if (v.at(mid) > v.at(mid + 1) &&
-                       v.at(mid) >
-                           v.at(mid - 1)) { // pivot but the largest element
-                pivot = mid + 1;
-                break;
-            } else if (v.at(mid) > v.at(high))
-                low = mid + 1;
-            else
-                high = mid - 1; // v.at(mid) < v.at(high)
-        }
-        if (pivot == -1) { // already sorted no rotation i.e k = 0
-            low = 0;
-            high = n - 1;
-            while (low <= high) {
-                int mid = low + ((high - low) / 2);
-                if (v.at(mid) == target)
-                    return mid;
-                else if (v.at(mid) > target)
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            //if found, stop
+            if(nums[mid] == target)return mid;
+            //identify the sorted half , is it the left half or the right half
+            //the lower value shall be smaller than the mid 
+            //there by we can say that the right half is sorted 
+            if(nums[low] <= nums[mid]){
+                //if left sorted
+                if(nums[low] <= target && target <= nums[mid]){
+                    //if true the target lies within this
+                    //we can remove the right half 
                     high = mid - 1;
-                else
+                }else{
+                    //remove the left half
                     low = mid + 1;
-            }
-            return -1;
-        }
-        // to find target
-        //  from pivot - end & from start - pivot are two different searching
-        //  area target can be in either of them not both thou
-        // therefore, we will start by traversing from 0 to pivot for target and
-        // apply bs in that
-        //  area; similary we will traverse the other area ; and apply bs to
-        //  reduce the search space and avoid O(N)
-        if (target >= v.at(0) && target <= v.at(pivot - 1)) {
-            low = 0;
-            high = pivot - 1;
-            while (low <= high) {
-                int mid = low + ((high - low) / 2);
-                if (v.at(mid) == target)
-                    return mid;
-                else if (v.at(mid) > target)
-                    high = mid - 1;
-                else
+                }
+            }else{
+                //if right half is sorted 
+                if(nums[mid] <= target && target <= nums[high]){
+                    //if true the target within this range
+                    //we can remove the left half
                     low = mid + 1;
-            }
-        } else {
-            low = pivot;
-            high = n - 1;
-            // applying bs in the second search area
-            while (low <= high) {
-                int mid = low + ((high - low) / 2);
-                if (v.at(mid) == target)
-                    return mid;
-                else if (v.at(mid) > target)
-                    high = mid - 1;
-                else
-                    low = mid + 1;
+                }else{
+                    //remove the right
+                    high = mid- 1;
+                }
             }
         }
         return -1;
