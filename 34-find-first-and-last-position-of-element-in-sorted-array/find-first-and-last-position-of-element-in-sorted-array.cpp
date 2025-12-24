882b7b240f14a4b3d902ec1359e70a60ab86c28a
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2025-12-24 10:57:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (16.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    int leftfind(vector<int>&nums, int target , int n){
-        int l = 0;
-        int h = n - 1;
-        int left = -1;
-        while(l <= h){
-            int mid = l +( (h - l) >> 1);
-            if(nums[mid] == target){
-                left = mid;
-                h = mid - 1;
-            }
-            else if(nums[mid] < target)  l = mid + 1;
-            else h = mid - 1;
-        }
-        return left;
-    }
-    int rightfind(vector<int>&nums, int target , int n){
-        int l = 0;
-        int h = n - 1;
-        int right = -1;
-        while(l <= h){
-            int mid = l + ((h - l) >> 1);
-            if(nums[mid] == target){
-                right = mid;
-                l = mid + 1;
-            }
-            else if(nums[mid] < target) l = mid + 1;
-            else h = mid - 1;
-        }
-        return right;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int leftO = leftfind(nums, target, n);
-        int rightO = rightfind(nums, target, n);
-        return {leftO, rightO};
+        if(nums.size() == 0)return {-1,-1};
+        int low = 0;
+        int high = nums.size() - 1;
+        int firstocc = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                firstocc = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target)low = mid + 1;
+            else high = mid - 1;
+        }
+        int lastocc = -1;
+        low = 0;
+        high = nums.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                lastocc = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target)low = mid + 1;
+            else high = mid - 1;
+        }
+        return {firstocc, lastocc};
     }
 };
