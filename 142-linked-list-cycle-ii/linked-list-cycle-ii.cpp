92564053fd7a7bc61adba58d4559988d14471bbd
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2026-01-03 16:05:59 +0530

    Time: 10 ms (46.99%) | Memory: 11.5 MB (22.44%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,21 +9,26 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        // if(head == NULL || head->next == NULL) return NULL;
+        // if(head->next == NULL)return NULL;
         ListNode* slow = head;
         ListNode* fast = head;
-        //phase 1 : detect cycle and stop when detected i.e slow == fast
+        bool flag = false;
+
         while(fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast)break;
+
+            if(slow == fast){
+                flag = true;
+                break;
+            }
         }
+        slow = head;
         if(fast == NULL || fast->next == NULL)return NULL;
-        //phase 2 : if they meet, find the start of cycle
-        while(head != slow){
-                head = head->next;
-                slow= slow->next;
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next;
         }
-        return head;
+        return slow;
     }
 };
