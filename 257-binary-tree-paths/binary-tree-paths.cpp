4432c980ffe09c67b06fe6b21ba18e1bb2e284bf
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2026-01-06 20:09:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.15%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    void paths(TreeNode* root, string s,vector<string>&ans){
-        if(root == NULL) return;
-        string a = to_string(root->val);
-        if(root->left == NULL && root->right == NULL){
-            s += a;
-            ans.push_back(s);
-            return;
-        }
-        paths(root->left, s+a+"->", ans);
-        paths(root->right, s+a+"->", ans);
+void helper(TreeNode* root, string s, vector<string>&st){
+    if(root == NULL)return;
+    string a = to_string(root->val);
+    if(root->left == NULL && root->right == NULL){
+        s += a;
+        st.push_back(s);
+        return;
     }
+    helper(root->left , s + a+"->" , st);
+    helper(root->right , s + a+"->" , st);
+
+}
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string>ans;
-        paths(root, "" ,ans);
-        return ans;
+        vector<string>st;
+        helper(root, "" , st);
+        return st;
     }
 };
