e16753b7f3c7dff2214a4f9950d7801e27493e40
priiikrrrr <129759603+priiikrrrr@users.noreply.github.com>
2026-01-14 20:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (73.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char , int> mp;
-        unordered_map<char , int> sp;
-        if(s.length()!= t.length())return false;
-        for(int i = 0 ; i < s.length() ; i++){
-            mp[s[i]]++;
+        if(s.length() != t.length())return false;
+        vector<int>arr(26,0);
+        for(int i = 0 ; i < s.size() ; i++){
+            arr[s.at(i) - 'a']++;
+            arr[t.at(i) - 'a']--;
         }
-        for(int i = 0 ; i < t.length() ; i++){
-            sp[t[i]]++;
-        }
-
-        for(auto i : mp){
-            char ch = i.first;
-            int frq = i.second;
-
-            if(sp.find(ch) != sp.end()){
-                int frq1 = sp[ch];// finding the frequency of the character ch , if we find it in string t.
-                //and now if the freq of that certain char found in t & s matches we return true, else false;
-                if(frq1 != frq)return false;
-            }else{
-                return false;
-            }
+        for(int j : arr){
+            if(j != 0)return false;
         }
         return true;
     }
